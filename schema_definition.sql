6ce34b8030a056c43209c3aed259400bc16b2656
Brian Fraser <fraserbn@gmail.com>
2018-09-17 23:04:34 +0200

    updated_at is also useful

--- schema_definition.sql
+++ schema_definition.sql
@@ -4,7 +4,8 @@ CREATE TABLE `discord_name_override` (
     `id` INTEGER PRIMARY KEY AUTOINCREMENT,
     `discord_id` TEXT NOT NULL UNIQUE,
     `nickname`   TEXT NOT NULL,
-    `row_created_at` DATETIME NOT NULL
+    `row_created_at` DATETIME NOT NULL,
+    `row_updated_at` DATETIME NOT NULL
 );
 DELETE FROM sqlite_sequence;
 COMMIT;
