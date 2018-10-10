82cf98883f7e2283727ab7fdaff3c76a89bc8f84
Brian Fraser <fraserbn@gmail.com>
2018-10-11 01:47:10 +0200

    Discord has special exceptions for URLs and metacharacters
    Two of them:
    
        1. A normal markdown metacharacter does not *start* a sequence
           if it is inside of an url.  So https://example.org/foo_bar_baz
           does not leave the "bar" in italics, but _https://_foo will
           leave the https:// in italics.
        2. A backlash inside a url is always translated into a forward
           slash, never as a 'quote this metacharacter'.  So
           https://example.org/foo\_bar will end up with a url like
           https://example.org/foo/_bar
    
    This means that we can't just quote a string for metacharacters --
    we need to quote everything except URLs.  Yaay.
    
    So this commit does that -- we apply no quoting to URLs (the explicit
    ones coming from <a>...</a>, anyway).

--- kolpublicmessage.go
+++ kolpublicmessage.go
@@ -6,6 +6,8 @@ import (
 
     "strings"
     "bytes"
+    "sort"
+
     "github.com/Hugmeir/kolgo"
 )
 
@@ -16,9 +18,10 @@ func EscapeDiscordMetaCharacters(s string) string {
 }
 
 var linkMatcher = regexp.MustCompile(`(?i)<a target=_blank href="([^"]+)"[^>]*><font[^>]+>[^<]+<[^>]+><\\?/a>`)
-func FixMangledChatLinks(a string) string {
+func FixMangledChatLinks(a string) (string, []string) {
     s := []byte(a)
 
+    urls := []string{}
     for max := 10; max > 1; max-- {
         loc := linkMatcher.FindSubmatchIndex(s);
         if len(loc) <= 0 {
@@ -29,6 +32,7 @@ func FixMangledChatLinks(a string) string {
         // Grab the url first
         urlRaw := make([]byte, loc[3] - loc[2])
         copy(urlRaw, s[loc[2]:loc[3]])
+        urls = append(urls, string(urlRaw))
 
         // Now get rid of the whole <a> eyesore:
         // The following is the go way of doing this
@@ -55,7 +59,7 @@ func FixMangledChatLinks(a string) string {
         }
     }
 
-    return string(s)
+    return string(s), urls
 }
 
 var effectMatcher  = regexp.MustCompile(`(?i)<img src="[^"]+12x12(heart|skull)\.[^"]+"[^>]*>`)
@@ -73,7 +77,7 @@ func HandleKoLPublicMessage(kol kolgo.KoLRelay, message kolgo.ChatMessage, effec
 
     wrapAround := make(map[string]bool, 3)
 
-    preparedMessage = FixMangledChatLinks(preparedMessage)
+    preparedMessage, urls := FixMangledChatLinks(preparedMessage)
 
     if strings.HasPrefix(preparedMessage, "<") {
         // golden text, chat effects, etc.
@@ -115,7 +119,65 @@ func HandleKoLPublicMessage(kol kolgo.KoLRelay, message kolgo.ChatMessage, effec
         }
     }
 
-    preparedMessage = EscapeDiscordMetaCharacters(preparedMessage)
+    // Sigh... Discord has SPECIAL ESCAPING EXCEPTIONS for urls.
+    // Consider this: https://foo.com/foo_bar_baz
+    // In normal markdown, that would leave the "bar" in italics.
+    // Discord grabs anything that looks like an url and does not
+    // apply markdown to them.
+    //
+    // Neat, right?  Except, it also has EXTRA MAGICAL BEHAVIOR,
+    // so that if you properly escape the url, ala
+    //  https://foo.com/foo\_bar\_baz
+    // What actually comes out is this horrible mess:
+    //  https://foo.com/foo/_bar/_baz
+    // So we need to escape metacharacters... except in urls.  Sigh.
+    if len(urls) > 0 {
+        sort.Slice(urls, func(i, j int) bool { return len(urls[i]) > len(urls[j]) })
+        skip := [][]int{}
+        b    := []byte(preparedMessage)
+        f    := make([]byte, len(b)*2)
+
+        for _, url := range urls {
+            offset := 0
+            for {
+                if offset >= len(b)-1 {
+                    break
+                }
+                foundFromOffset := bytes.Index(b[offset:], []byte(url))
+                if foundFromOffset == -1 {
+                    break
+                }
+                skip = append(skip, []int{offset + foundFromOffset, offset + foundFromOffset + len(url)})
+                offset = offset + foundFromOffset + len(url)
+            }
+        }
+
+        // Sort the indexes
+        sort.Slice(skip, func(i, j int) bool { return skip[i][0] < skip[j][0] })
+
+        start := 0
+        for _, t := range skip {
+            // Duplicate strings
+            if start > t[0] {
+                continue
+            }
+
+            before := b[start:t[0]]
+            before = discordMeta.ReplaceAll(before, []byte("\\$1"))
+            f = append(f, before...)
+            f = append(f, b[t[0]:t[1]]...)
+            start = t[1]
+        }
+
+        if start < len(b) {
+            tail := b[start:]
+            tail  = discordMeta.ReplaceAll(tail, []byte("\\$1"))
+            f = append(f, tail...)
+        }
+        preparedMessage = string(f)
+    } else {
+        preparedMessage = EscapeDiscordMetaCharacters(preparedMessage)
+    }
 
     preparedMessage = captureItalics.ReplaceAllString(preparedMessage, `*$1*`)
 
